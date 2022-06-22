88780414b888f0f7c076f749081d9b0e04929f5a
Tyler <tyler_mac@Rainbow-Trains-MacBook-Pro.local>
2022-06-22 16:10:52 -0400

    f

--- src/components/FriendForm.js
+++ src/components/FriendForm.js
@@ -9,12 +9,18 @@ export default function FriendForm(props) {
     // a) pull the name of the input from the event object
     // b) pull the value of the input from the event object
     // c) use the `update` callback coming in through props
+    const name = evt.target.name;
+    const value = evt.target;
+
+    update(name, value)
   }
 
   const onSubmit = evt => {
     // 🔥 STEP 7 - IMPLEMENT the submit handler
     // a) don't allow the browser to reload!
     // c) use the `submit` callback coming in through props
+    evt.preventDefault();
+    submit();
   }
 
   return (
@@ -28,10 +34,25 @@ export default function FriendForm(props) {
               Controlled inputs need `value` and `onChange` props.
               Inputs render what they're told - their current value comes from app state.
               At each keystroke, a change handler fires to change app state. */}
+          <input
+            name="username"
+            type="text"
+            placeholder="enter a username"
+            maxLength="30"
+            value={values.username}
+            onChange={onChange}
+          />
         </label>
 
         <label>Email
           {/* 🔥 STEP 4 - Make an input of type `email` or `text` for email. */}
+          <input
+            name="email"
+            type="email"
+            placeholder="enter a(n) email"
+            onChange={onChange}
+            value={values.email}
+          />
         </label>
 
         {/* ////////// DROPDOWN ////////// */}
@@ -39,10 +60,14 @@ export default function FriendForm(props) {
         {/* ////////// DROPDOWN ////////// */}
         <label>Role
           {/* 🔥 STEP 5 - Make dropdown for role. */}
+          <select value={values.role} name="role" onChange={onChange}>
+            <option value=''>--select role--</option>
+            <option value='student'>student</option>
+          </select>
         </label>
 
         <div className='submit'>
-          <button>submit</button>
+          <button disabled={!values.username || !values.email || !values.role}>submit</button>
         </div>
       </div>
     </form>
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { render } from 'react-dom'
 // 👉 App contains a more sophisticated form we'll flesh out later
 import App from './components/App'
@@ -11,7 +11,52 @@ const petsList = [
 ]
 
 function SimpleForm() {
-  return <div>Ready to start GP!</div>
+  const [pets, setPets] = useState(petsList);
+  const [formValue, setFormValues] = useState({
+    petName: "",
+    petType: ""
+  })
+
+  const change = function (val) {
+    console.log(val.target.name, val.target.value)
+    setFormValues({ ...formValue, [val.target.name]: val.target.value })
+  }
+
+  const submit = (val) => {
+    val.preventDefault();
+    const newPet = {
+      petName: formValue.petName.trim(),
+      petType: formValue.petType.trim(),
+    }
+
+    setPets(pets.concat(newPet))
+    setFormValues({ petName: "", petType: "" })
+  }
+
+  return (
+    <div className="container">
+      {
+        petsList.map(pet => {
+          petsList.map(pet, idx =>
+            <p key={idx}>PetName: {pet.petName} petType: {pet.petType} </p>)
+        })
+      }
+
+      <form onSubmit={submit}>
+        <input type="text" name='petName'
+          onChange={change}
+          value={formValue.petName}
+        />
+
+        <input type="text" name='petType'
+          onChange={change}
+          value={formValue.petType}
+        />
+
+        <input type='submit' value="HAVE A PET" />
+      </form>
+    </div>
+  )
 }
 
 render(
